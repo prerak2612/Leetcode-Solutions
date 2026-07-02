17c40ff4e760bd5843113577c3757ec016427038
prerak2612 <149291735+prerak2612@users.noreply.github.com>
2026-07-02 12:41:09 +0530

    Add solution for 0000. Guess Number Higher or Lower

--- Problems/0000-guess-number-higher-or-lower/solution.py
+++ Problems/0000-guess-number-higher-or-lower/solution.py
@@ -7,11 +7,3 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        s = 1
-        e = n
-        while(s <= e):
-            mid = (s+e) // 2
-            if(guess(mid) == 0):
-                return mid
-            elif guess(mid) == -1:
-                e = mid - 1
