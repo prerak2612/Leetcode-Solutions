0d28fed8a5ae052cd76b1b45745b7b36c6c62638
prerak2612 <149291735+prerak2612@users.noreply.github.com>
2026-07-02 12:50:40 +0530

    Add solution for 0000. Group Anagrams

--- Problems/0000-group-anagrams/solution.py
+++ Problems/0000-group-anagrams/solution.py
@@ -1,13 +1,2 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = defaultdict(list)
-        for i in strs:
-            temp = ''.join(sorted(i))
-            #aet
-            dic[temp].append(i)
-            #aet:eat
-        res = []
-        for group in dic.values():
-            res.append(group)
-        return res
