ec84fd932da431fa2407a87c2250faa9f34db04b
prerak2612 <149291735+prerak2612@users.noreply.github.com>
2026-07-02 12:04:48 +0530

    Add solution for 0000. Swap Nodes in Pairs

--- Problems/0000-swap-nodes-in-pairs/solution.py
+++ Problems/0000-swap-nodes-in-pairs/solution.py
@@ -5,11 +5,3 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummyNode = ListNode(-1)
-        dummyNode.next = head
-        prev = dummyNode
-        while prev and prev.next and prev.next.next:
-            first = prev.next
-            second = first.next
-            newpair = second.next
-            prev.next =
