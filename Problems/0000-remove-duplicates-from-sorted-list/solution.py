e48813f98350e0870e0dbe6e4c02e1cee029742a
prerak2612 <149291735+prerak2612@users.noreply.github.com>
2026-07-02 12:22:03 +0530

    Add solution for 0000. Remove Duplicates from Sorted List

--- Problems/0000-remove-duplicates-from-sorted-list/solution.py
+++ Problems/0000-remove-duplicates-from-sorted-list/solution.py
@@ -8,3 +8,9 @@ class Solution:
         if head is None:
             return None
         curr = head
+        while curr and curr.next:
+            if(curr.val == curr.next.val):
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
